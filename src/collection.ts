7cd356f3db67769b26462623783cdc29d9f774c3
KaimDev <johanedfb@gmail.com>
2023-05-26 20:30:53 -0600

    Refactor code   * package-lock.json   * package.json
    src/
      * collection.ts(insertOne, insertMany): Implement

--- src/collection.ts
+++ src/collection.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4, v4 } from 'uuid';
+
 import { Document } from "./document";
 import { IDeleteManyResponse } from "./interface/delete-many-response.interface";
 import { IDeleteOneResponse } from "./interface/delete-one-response.interface";
@@ -5,6 +7,7 @@ import { IInsertManyResponse } from "./interface/insert-many-response.interface"
 import { IInsertOneResponse } from "./interface/insert-one-response.interface";
 import { IUpdateManyResponse } from "./interface/update-many-response.interface";
 import { IUpdateOneResponse } from "./interface/update-one-response.interface";
+
 
 export class Collection {
 
@@ -15,13 +18,13 @@ export class Collection {
             
             let count: number = 0;
 
-            if (query != undefined && this.documents != undefined) {
+            if (query && this.documents) {
 
                 for (const key in query) {
 
                     for (let document of this.documents) {
 
-                        if ((document as Object).hasOwnProperty(key)) {
+                        if (document.hasOwnProperty(key)) {
                             
                             const valueOfDocumentKey: any = (document as any)[key];
                             const valueOfQueryKey: any = (query as any)[key];
@@ -39,7 +42,7 @@ export class Collection {
 
                 resolve(count);
             }
-            else if (this.documents != undefined) {
+            else if (this.documents) {
 
                 count = this.documents.length;
 
@@ -56,7 +59,7 @@ export class Collection {
 
         return new Promise((resolve, reject) => {
 
-            if (query != undefined && this.documents != undefined) {
+            if (query && this.documents) {
                 
                 let matches: object[] = [];
 
@@ -64,7 +67,7 @@ export class Collection {
 
                     for (let document of this.documents) {
 
-                        if ((document as Object).hasOwnProperty(key)) {
+                        if (document.hasOwnProperty(key)) {
 
                             const valueOfDocumentKey: any = (document as any)[key];
                             const valueOfQueryKey: any = (query as any)[key];
@@ -79,7 +82,7 @@ export class Collection {
 
                 resolve(matches);
             }
-            else if (this.documents != undefined) {
+            else if (this.documents) {
 
                 resolve(this.documents)
             }
@@ -90,17 +93,18 @@ export class Collection {
 
         })
     }
+
     public findOne(query?: object): Promise<object | null> {
 
         return new Promise((resolve, reject) => {
 
-            if (query != undefined && this.documents != undefined) {
+            if (query && this.documents) {
 
                 for (const key in query) {
 
                     for (let document of this.documents) {
 
-                        if ((document as Object).hasOwnProperty(key)) {
+                        if (document.hasOwnProperty(key)) {
 
                             const valueOfDocumentKey: any = (document as any)[key];
                             const valueOfQueryKey: any = (query as any)[key];
@@ -120,18 +124,47 @@ export class Collection {
 
         })
     }
-    public insertOne(query: object): Promise<IInsertOneResponse> {
 
+    public insertOne(query: object): Promise<IInsertOneResponse> {
         return new Promise((resolve, reject) => {
 
-            
+            if (query) {
+                let document: Document = new Document()
+                document._id = v4();
+                Object.setPrototypeOf(document, query)
+                this.documents!.push(document);
+
+                resolve({ success: true, insertedId: document._id });
+            }
+            else {
+                throw new Error("Invalid query");
+            }
         })
     }
+
     public insertMany(query: object[]): Promise<IInsertManyResponse> {
-
         return new Promise((resolve, reject) => {
+            let id_collection: string[] = [];
 
-            
+            if (query) {
+                query.forEach((queryItem: Object) => {
+                    let document: Document = new Document();
+                    document._id = v4();
+                    id_collection.push(document._id);
+                    Object.setPrototypeOf(document, queryItem);
+                    this.documents!.push(document);
+                })
+            }
+            else {
+                throw new Error("Invalid queries")
+            }
+
+            const response: IInsertManyResponse = {
+                success: true,
+                insertedIds: id_collection
+            }
+
+            resolve(response);
         })
     }
     public updateOne(query: object, update: object): Promise<IUpdateOneResponse> {
